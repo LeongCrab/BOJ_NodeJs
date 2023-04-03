07f7683380fef5771c223f3eb3d24da7f244b571
LeongCrab <dladuscjf8@gmail.com>
2023-04-03 19:13:08 +0900

    first commit

---
+++ template/solution.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+// one line input
+let input = fs.readFileSync('./template/input.txt').toString().split(' ');
+// multi line input
+//let input = fs.readFileSync('./template/input.txt').toString().split('\n');
+//BOJ 체점시 input 파일 경로는 /dev/stdin으로 변경해주어야 한다.
+
+console.log(input);
